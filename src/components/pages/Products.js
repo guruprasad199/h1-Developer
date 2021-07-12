00e99187afd708d93a11a87014768010980ff80a
Guruprasad <guruprasadsali378@gmail.com>
2021-07-12 21:54:35 +0530

    footer

--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import LocalClint from '../LocalClint'
+import Footer from "../Footer"
 
 export default class Products extends Component {
     render() {
         return (
             <div>
                 <LocalClint/>
+                <Footer/>
             </div>
         )
     }
